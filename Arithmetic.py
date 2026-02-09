0c133a5bc71597b732fc27f11987f0c8e5f38bdd
Bhagyashri Dughad <bhagyshri9090@gmail.com>
2026-02-09 17:02:39 +0530

    Operator

---
+++ Arithmetic.py
@@ -0,0 +1,18 @@
+# Arithmetic Operator
+
+a=15
+b=4
+print("Addition:",a+b)          #Addition
+print("Subtraction:",a-b)       #Subtraction
+print("product:",a*b)           #Product
+print("Division:",a/b)          #Division
+print("Floor division:",a//b)   #Floor division
+print("Modulus:",a%b)           #Modulus
+print("Exponent:",a**b)         #Exponent
+
+
+# add two numbers without  "+" operator.
+
+a=9
+b=5
+print(a-(-b))
